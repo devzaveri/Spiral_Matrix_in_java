081c40ca849430692bca4641c3b06ab9e6a86b54
Dev Zaveri <devsoni5880@gmail.com>
2023-01-31 17:51:18 +0530

    (ignore)

--- Spiral_Matrix_in_java__2.java
+++ Spiral_Matrix_in_java__2.java
@@ -15,24 +15,36 @@ public class Spiral_Matrix_in_java__2 {
       int B = n - 1;
       int L = 0;
       int R = n - 1;
+      int dir = 0;
       
       while (T <= B && L <= R) {
+        if(dir == 0){
         for (int i = L; i <= R; i++) {
           matrix[T][i] = count++;
         }
         T++;
+        dir = 1;
+      }
+      else if(dir == 1){
         for (int i = T; i <= B; i++) {
           matrix[i][R] = count++;
         }
         R--;
+        dir = 2;
+      }
+      else if(dir == 2){
         for (int i = R; i >= L; i--) {
           matrix[B][i] = count++;
         }
         B--;
+        dir = 3;
+      }else {
         for (int i = B; i >= T; i--) {
           matrix[i][L] = count++;
         }
         L++;
+        dir = 0;
+      }
       }
       return matrix;
     }
